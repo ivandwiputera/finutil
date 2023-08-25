6441d58bb40f827c57c6dfa20fbcac2c85a5ae0c
Ivan Dwiputera <ivan.dwiputera@gmail.com>
2023-08-25 23:33:40 +0700

    setup hash router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Home from "scenes/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter  as Router, Routes, Route } from "react-router-dom";
 import { ScreenType } from "ScreenType";
 import EmergencyFundsCalculator from "scenes/EmergencyFundsCalculator/EmergencyFundsCalculator";
 import Budget503020Calculator from "scenes/Budget503020Calculator/Budget503020Calculator";
 
 function App() {
   return (
-    <div className="app" baseName={process.env.PUBLIC_URL}>
+    <div className="app">
       <div className="app-container">
-        <Router>
+        <Router baseName={process.env.PUBLIC_URL}>
           <Routes>
             <Route path={ScreenType.budget503020Calculator} element={<Budget503020Calculator />} />
             <Route path={ScreenType.emergencyFundsCalculator} element={<EmergencyFundsCalculator />} />
